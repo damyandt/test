d5048e995fa19475498b1aee76ec439c13bb5f0b
Damian Todorov <damqn_dimitrov1111@icloud.com>
2024-02-15 12:52:36 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,16 @@
 function raft(N, K, weights) {
     weights.sort((a, b) => b - a);
-
-    let maxWeight = weights[0];
-    let minCapacity = weights[0] + 1;
-
+    let minCapacity = weights[0];
     let weightsCopy = [];
-    for (let i = 0; i < weights.length; i++) {
+    let boolean = true;
+    let times = 0;
+    let sum = 0;
+
+    for (let i = 0; i < N; i++) {
         weightsCopy.push(weights[i])
     }
     weightsCopy.sort((a, b) => b - a);
 
-
-    let boolean = true;
-    let times = 0;
-    let sum = 0;
     while (boolean) {
 
         for (let i = 0; i < weightsCopy.length; i++) {
@@ -22,13 +19,7 @@ function raft(N, K, weights) {
             for (let j = 0; j < weightsCopy.length; j++) {
                 if ((sum + weightsCopy[j]) <= minCapacity) {
                     sum += weightsCopy[j];
-                    let after = weightsCopy.slice(j + 1)
-                    let before = weightsCopy.slice(0, j)
-                    weightsCopy = before;
-                    for (let i = 0; i < after.length; i++) {
-                        weightsCopy.push(after[i])
-                    }
-                    weightsCopy.sort((a, b) => b - a);
+                    weightsCopy.splice(j, 1);
                     j--;
                 }
                 if (times > K && boolean) {
@@ -43,33 +34,30 @@ function raft(N, K, weights) {
                 times++;
             }
 
-
-
             if (times > K && boolean) {
                 weightsCopy = [];
                 for (let i = 0; i < weights.length; i++) {
                     weightsCopy.push(weights[i])
                 }
                 weightsCopy.sort((a, b) => b - a);
-
                 minCapacity++;
                 times = 0;
             }
-
 
             if (weightsCopy.length == 0) {
                 console.log(minCapacity)
                 boolean = false;
                 break;
             }
-
         }
-
     }
 }
 
-let N = 6;
-let K = 2;
-let weights = [52, 17946, 27160, 387, 17346, 27505, 20816, 20577, 10961, 6021, 5262, 28278, 24163, 931, 11003, 19738, 17914, 1683, 10320, 10475];
+// let N = 26;
+// let K = 5;
+// let weights = [52, 17946, 27160, 387, 17346, 27505, 20816, 20577, 10961, 6021, 5262, 28278, 24163, 931, 11003, 19738, 17914, 1683, 10320, 10475];
+ let N = 7;
+ let K = 2;
+ let weights = [2, 15, 20, 5, 8, 10, 12];
 
 raft(N, K, weights);
